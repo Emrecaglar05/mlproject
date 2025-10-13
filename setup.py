7ff5ee7e78c398e9ebdc07cb750ca6ccd6d81a2c
John Doe <emrecaglar0505@gmail.com>
2025-10-13 18:34:30 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name = "mlproject",
+version="0.0.1",
+author="Emre",
+author_email="emrecaglar0505@gmail.com",
+packages=find_packages(),
+requires=["pandas", "numpy", "seaborn"]
+
+)
